daae5289be3b84b6a347100189fb90cf6eae16e8
vpsbotnew <121183724+vpsbotnew@users.noreply.github.com>
2023-12-05 21:54:29 +0630

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config(object):
       BOT_TOKEN = "5244640547:AAHCDdX2pYDeJ_dHzTp6EJGeOg1X-4PhjJA"
       API_ID = int("20512581")
       API_HASH = "86e23f5f868190ca18c2a542247e51e6"
-      CAPTION_TEXT = "\n[👉YN Main Movie ချန်နယ် Joinရန်](https://t.me/YNMOVIETWO)\n[👉YN VIP ချန်နယ် Joinရန်](https://t.me/YN_VIP_Series_ListAndPoster)\n[👉YN Channel Link အားလုံး Joinရန်](https://t.me/YoeNaungAllMovieList/133)\n[🔎Movie တောင်းရန်နှင့်ရှာဖွေရန်ဂရု 🔎](https://t.me/+XTScHquCH0A0ZTQ1)\n[🔎Series တောင်းရန်နှင့်ရှာဖွေရန်ဂရု🔎](https://t.me/YNMovie_SeriesBank)"
+      CAPTION_TEXT = "\n[👉YN Main Movie ချန်နယ် Joinရန်](https://t.me/YNMOVIEONE)\n[👉YN VIP ချန်နယ် Joinရန်](https://t.me/YN_VIP_Series_ListAndPoster)\n[👉YN Channel Link အားလုံး Joinရန်](https://t.me/addlist/UuYiNy4BLLw5Mzg1)\n[🔎Movie တောင်းရန်နှင့်ရှာဖွေရန်ဂရု 🔎](https://t.me/+XTScHquCH0A0ZTQ1)\n[🔎Series တောင်းရန်နှင့်ရှာဖွေရန်ဂရု🔎](https://t.me/YNMovie_SeriesBank)"
       CAPTION_POSITION = "bottom"
       ADMIN_USERNAME = "YoeNaung"
